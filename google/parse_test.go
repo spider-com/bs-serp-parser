cea6b7b881c99302406b1b4ad94102165a073544
Taras Matsyk <tamatsyk@gmail.com>
2020-10-07 18:45:37 +0300

    Fix title/description parser

--- google/parse_test.go
+++ google/parse_test.go
@@ -14,30 +14,24 @@ func TestParse(t *testing.T) {
 	res, err := parse(bytes.NewReader(googleDoc))
 	require.Nil(t, err)
 
-	require.Equal(t, int64(3530000000), res.TotalResultCount)
+	require.Equal(t, int64(1440000000), res.TotalResultCount)
 
-	require.Equal(t, 12, len(res.OrganicItems))
-	require.Equal(t, "https://www.techradar.com/news/best-iphone", res.OrganicItems[0].URL)
-	require.Equal(t, "Jul 15, 2020 - Best iPhone: which one should you buy today? iPhone 11. (Image credit: Apple). 1. iPhone 11.", res.OrganicItems[0].Description)
-	require.Equal(t, "www.techradar.com › news › best-iphonewww.techradar.com › news › best-iphone", res.OrganicItems[0].DisplayURL)
+	require.Equal(t, 9, len(res.OrganicItems))
+	require.Equal(t, "https://www.pizzahut.com/", res.OrganicItems[0].URL)
+	require.NotEmpty(t, res.OrganicItems[0].Description)
+	require.Equal(t, "www.pizzahut.comwww.pizzahut.com", res.OrganicItems[0].DisplayURL)
 	require.Equal(t, 0, res.OrganicItems[0].Position)
 	require.Equal(t, 0, res.OrganicItems[0].PositionOverall)
-	require.Equal(t, "Best iPhone 2020: which Apple phone is the top choice for ...", res.OrganicItems[0].Title)
+	require.Equal(t, "Pizza Hut: Pizza Delivery | Pizza Carryout | Coupons | Wings ...", res.OrganicItems[0].Title)
 
-	require.Equal(t, 3, len(res.RelatedQuestions))
-	require.Equal(t, "Which is the best iPhone to buy?", res.RelatedQuestions[0])
+	require.Equal(t, 4, len(res.RelatedQuestions))
+	require.Equal(t, "What defines a pizza?", res.RelatedQuestions[0])
 
-	require.Equal(t, 4, len(res.TopPLAItems))
-	require.Equal(t, "Мобильный телефон Apple iPhone Xr 64GB", res.TopPLAItems[0].Title)
-	require.Equal(t, "https://rozetka.com.ua/apple_mry52/p141517594/", res.TopPLAItems[0].URL)
-	require.Equal(t, "UAH 21,499.00", res.TopPLAItems[0].Price)
-	require.Equal(t, "ROZETKA", res.TopPLAItems[0].Source)
+	require.Equal(t, 0, len(res.TopPLAItems))
 
 	require.Equal(t, int64(1), res.Pagination.Current)
-	require.Equal(t, "https://www.google.com/search?q=iphone+which+one&ei=dLwqX9LSO5G53AOaoa-QDw&start=10&sa=N&ved=2ahUKEwjS3P_QnYTrAhWRHHcKHZrQC_IQ8NMDegQIDhBl", res.Pagination.Next)
+	require.Equal(t, "https://www.google.com/search?q=pizza&ei=X919X822G9a5tAbXkbDgBg&start=10&sa=N&ved=2ahUKEwiN4obZ5KLsAhXWHM0KHdcIDGwQ8NMDegQIAxBC", res.Pagination.Next)
 	require.Equal(t, 9, len(res.Pagination.OtherPages))
 
-	require.Equal(t, 1, len(res.PaidItems))
-	require.Equal(t, "Айфоны в Rozetka - Купить, Отзывы, Цена - rozetka.com.ua", res.PaidItems[0].Title)
-	require.Equal(t, "https://rozetka.com.ua/mobile-phones/c80003/preset=smartfon;producer=apple/", res.PaidItems[0].URL)
+	require.Equal(t, 0, len(res.PaidItems))
 }
